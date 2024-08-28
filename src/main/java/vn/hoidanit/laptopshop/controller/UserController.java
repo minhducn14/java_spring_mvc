5509b278065a590a5075e27cd51c3a984f75ed03
unknown <vunguyenminhduc1410@gmail.com>
2024-08-28 22:29:55 +0700

    video 49+50+51+52

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,19 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String hello = this.userService.handlerHello();
+        model.addAttribute("hello", hello);
         return "hello";
     }
 
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        String test = this.userService.handlerHello();
+        model.addAttribute("minhduc", test);
+        model.addAttribute("form", "from controller with model");
+        return "admin/user/create";
+    }
 }
 
 // @RestController
