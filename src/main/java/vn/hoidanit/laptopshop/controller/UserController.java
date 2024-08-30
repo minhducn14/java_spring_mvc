73ea2d2cbb238827e5a2c46405ff6ba60cbdee28
unknown <vunguyenminhduc1410@gmail.com>
2024-08-30 10:15:29 +0700

    Video 61

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +25,11 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
+        List<User> users = this.userService.getAllUser();
+        System.out.println("Users: " + users);
+
+        List<User> usersByEmail = this.userService.getAllUserByEmail("2@gmail.com");
+        System.out.println("Users by email: " + usersByEmail);
         model.addAttribute("hello", "hello");
         return "hello";
     }
--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.laptopshop.domain.User;
@@ -16,6 +18,14 @@ public class UserService {
 
     public String handlerHello() {
         return "Hello";
+    }
+
+    public List<User> getAllUser() {
+        return this.userRepository.findAll();
+    }
+
+    public List<User> getAllUserByEmail(String email) {
+        return this.userRepository.findByEmail(email);
     }
 
     public User handlerSaveUser(User user) {
