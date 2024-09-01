b239180e815260d5c7ce81f6985a61b0bf3b1236
unknown <vunguyenminhduc1410@gmail.com>
2024-09-01 22:44:10 +0700

    Video 119

--- src/main/java/vn/hoidanit/laptopshop/service/CustomUserDetailsService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/CustomUserDetailsService.java
@@ -22,13 +22,14 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         vn.hoidanit.laptopshop.domain.User user = this.userService.getUserByEmail(username);
         if (user == null) {
-            throw new UsernameNotFoundException("User not found");
+            throw new UsernameNotFoundException("user not found");
         }
 
         return new User(
                 user.getEmail(),
                 user.getPassword(),
-                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().getName())));
+
     }
 
 }
