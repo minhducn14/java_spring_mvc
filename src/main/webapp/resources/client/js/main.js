ec8280d4dfb8a2ef833df67ffdb24eebf1beed68
unknown <vunguyenminhduc1410@gmail.com>
2024-09-03 11:34:00 +0700

    Video 144

--- src/main/webapp/resources/client/js/main.js
+++ src/main/webapp/resources/client/js/main.js
@@ -118,6 +118,7 @@
         $('.btn-play').click(function () {
             $videoSrc = $(this).data("src");
         });
+        console.log($videoSrc);
 
         $('#videoModal').on('shown.bs.modal', function (e) {
             $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
@@ -126,6 +127,20 @@
         $('#videoModal').on('hide.bs.modal', function (e) {
             $("#video").attr('src', $videoSrc);
         })
+
+        //add active class to header
+        const navElement = $("#navbarCollapse");
+        const currentUrl = window.location.pathname;
+        navElement.find('a.nav-link').each(function () {
+            const link = $(this); // Get the current link in the loop
+            const href = link.attr('href'); // Get the href attribute of the link
+
+            if (href === currentUrl) {
+                link.addClass('active'); // Add 'active' class if the href matches the current URL
+            } else {
+                link.removeClass('active'); // Remove 'active' class if the href does not match
+            }
+        });
     });
 
 
@@ -146,12 +161,10 @@
     //     button.parent().parent().find('input').val(newVal);
     // });
     $('.quantity button').on('click', function () {
-
         let change = 0;
 
         var button = $(this);
         var oldValue = button.parent().parent().find('input').val();
-
         if (button.hasClass('btn-plus')) {
             var newVal = parseFloat(oldValue) + 1;
             change = 1;
